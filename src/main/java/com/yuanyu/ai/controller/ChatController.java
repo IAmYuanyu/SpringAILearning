95709e2d18f0c782bf74849f8dd3672c18086eaa
缘鱼 <15557773+iamyuanyu@user.noreply.gitee.com>
2026-03-09 21:57:09 +0800

    实现不同会话间相互隔离

--- src/main/java/com/yuanyu/ai/controller/ChatController.java
+++ src/main/java/com/yuanyu/ai/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.yuanyu.ai.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,10 @@ public class ChatController {
     private final ChatClient chatClient;
 
     @RequestMapping(value = "/chat", produces = "text/html;charset=UTF-8")
-    public Flux<String> chat(@RequestParam String prompt) {
+    public Flux<String> chat(@RequestParam String prompt, @RequestParam String chatId) {
         return chatClient.prompt()
                 .user(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId))
                 .stream()
                 .content();
     }
