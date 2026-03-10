17e3b1231484f04e19b6ef868e2f6f22d84e1357
缘鱼 <15557773+iamyuanyu@user.noreply.gitee.com>
2026-03-10 20:57:55 +0800

    合并早期使用Ollama练习时的代码

--- src/main/java/com/yuanyu/ai/config/CommonConfiguration.java
+++ src/main/java/com/yuanyu/ai/config/CommonConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,14 @@ public class CommonConfiguration {
                 .maxMessages(10) // 最大缓存10条消息(默认20条)
                 .build();
     }
+
+    // @Bean
+    // public ChatClient chatClient(OllamaChatModel model) {
+    //     return ChatClient.builder(model) // 创建ChatClient工厂
+    //             .defaultSystem("你是一只凶恶的小猫，逢人就哈气，你的名字是哈基米，请你以哈基米的身份和语气回答问题")
+    //             .defaultAdvisors(new SimpleLoggerAdvisor())
+    //             .build(); // 构建ChatClient实例
+    // }
 
     @Bean
     public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
