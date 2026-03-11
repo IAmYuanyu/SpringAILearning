076054d679b5f3cfbe883d947aa89042a9eda2b2
缘鱼 <15557773+iamyuanyu@user.noreply.gitee.com>
2026-03-11 18:03:45 +0800

    编写前端黑马客服需要调用的Controller，并在启动类上加上@MapperScan确保能扫描到定义的mapper方法

--- src/main/java/com/yuanyu/ai/SpringAiLearningApplication.java
+++ src/main/java/com/yuanyu/ai/SpringAiLearningApplication.java
@@ -1,8 +1,10 @@
 package com.yuanyu.ai;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.yuanyu.ai.mapper")
 @SpringBootApplication
 public class SpringAiLearningApplication {
 
---
+++ src/main/java/com/yuanyu/ai/controller/CustomerServiceController.java
@@ -0,0 +1,36 @@
+package com.yuanyu.ai.controller;
+
+import com.yuanyu.ai.config.ChatClientFactory;
+import com.yuanyu.ai.constant.TypeConstants;
+import com.yuanyu.ai.repository.ChatHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/ai")
+@RequiredArgsConstructor
+public class CustomerServiceController {
+    // private final ChatClient serviceChatClient;
+    private final ChatClientFactory chatClientFactory;
+
+    private final ChatHistoryRepository chatHistoryRepository;
+
+
+    @RequestMapping(value = "/service", produces = "text/html;charset=UTF-8")
+    public Flux<String> service(@RequestParam String prompt, @RequestParam String chatId) {
+        // 保存会话id
+        chatHistoryRepository.save(TypeConstants.SERVICE, chatId); // 记得改为TypeConstants.SERVICE
+        // 请求模型
+        ChatClient serviceChatClient = chatClientFactory.getServiceChatClient();
+        return serviceChatClient.prompt()
+                .user(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId))
+                .stream()
+                .content();
+    }
+}
